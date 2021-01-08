3a2aca907525c14dd8add197627d9d75ee6a02bb
rnp39 <rnp39@cornell.edu>
2021-01-07 17:51:31 -0800

    some comp app refactor

--- Applicative.v
+++ Applicative.v
@@ -241,187 +241,7 @@ Module ListPlayground.
 
   Compute pure mult <*> [2;3;4] <*> pure 4.
   Compute mult <$> [2;3;4;5] <*> pure 4.
-
-  (* Composing Applicatives is tricky. *)
-(*
-  Fixpoint list_option_map {A B : Type} (fs : list (option (A -> B)))
-           (a : list (option A)) : list (option B) :=
-    match fs with
-    | [] => []
-    | None   :: t => list_option_map t a
-    | Some f :: t => fmap f <$> a ++ list_option_map t a
-    end.
-
-  Fixpoint list_option_map' {A B : Type} (fs : list (option (A -> B)))
-           (a : list (option A)) : list (option B) :=
-    match fs with
-    | []     => []
-    | fo :: t => pure (fapp fo) <*> a ++ list_option_map' t a
-    end.
-
-  Definition list_option_map'' {A B : Type} (fs : list (option (A -> B)))
-             (a : list (option A)) : list (option B) :=
-    concat ((fun fo => fapp (pure (fapp fo)) <$> a) <$> fs).
-
-  Fixpoint list_option_map''' {A B : Type} (fs : list (option (A -> B)))
-           (a : list (option A)) : list (option B) :=
-    match a with
-    | []       => []
-    | None   :: t => list_option_map''' fs t
-    | Some h :: t => fmap (fun f => f h) <$> fs ++ list_option_map''' fs t
-    end.
-
-  Fixpoint list_option_map'''' {A B : Type} (fs : list (option (A -> B)))
-           (a : list (option A)) : list (option B) :=
-    match a with
-    | []     => []
-    | ho :: t => (fun f => f <*> ho) <$> fs ++ list_option_map'''' fs t
-    end.
-
-  Definition list_option_map''''' {A B : Type} (fs : list (option (A -> B)))
-             (a : list (option A)) : list (option B) :=
-    concat ((fun ho => (fun f => f <*> ho) <$> fs) <$> a).
-  (** [ fun f  => f <*> ho                        : option (A -> B) -> option B]
-      [(fun f  => f <*> ho) <$> fs                : option B]
-      [ fun ho => (fun f => f <*> ho)                : option A -> list (option B)]
-      [(fun ho => (fun f => f <*> ho) <$> fs) <$> a  : list (option B)]
-      [(fun ho => (fun f => f <*> ho) <$> fs) <$> a) : list (list (option B))] *)
-
-  (** Definition fapp {A B : Type} (fs : list (A -> B)) (xs : list A) : list B :=
-      concat ((fun f => f <$> xs) <$> fs).
-      fs <*> xs = concat ((fun f => f <$> xs) <$> fs). *)
-
-  Definition maybe_fapp {A B : Type} (f : option (list (A -> B)))
-             (oa : option (list A)) : option (list B) :=
-    match f, oa with
-    | None, _ => None
-    | _, None => None
-    | Some f, Some a => Some (f <*> a)
-    end.
-
-  Definition maybe_fapp' {A B : Type} (f : option (list (A -> B)))
-             (oa : option (list A)) : option (list B) :=
-    match f with
-    | None => None
-    | Some f => (fun a => f <*> a) <$> oa
-    end.
-
-  Definition maybe_fapp''' {A B : Type} (f : option (list (A -> B)))
-             (oa : option (list A)) : option (list B) :=
-    match oa with
-    | None => None
-    | Some a => (fun h => h <*> a) <$> f
-    end.
-
-  (* Why was this so hard to come up with? *)
-  Definition maybe_fapp'''' {A B : Type} (fo : option (list (A -> B)))
-             (ao : option (list A)) : option (list B) := fapp <$> fo <*> ao.
-
-  Unset Printing Notations.
-  Print maybe_fapp''''.
-
-  Definition maybe_fapp''''' {A B : Type} (fo : option (list (A -> B)))
-             (ao : option (list A)) : option (list B) :=
-    AOption.fapp (AOption.fmap AList.fapp fo) ao.
-
-  Set Printing Notations.
-
-  Definition list_option_map'''''' {A B : Type} (fs : list (option (A -> B)))
-             (a : list (option A)) : list (option B) := fapp <$> fs <*> a.
-*)
 End ListPlayground.
-
-(** Applicative Composition *)
-Module ApplicativeCompose (Q R : ApplicativeSpec) <: ApplicativeSpec.
-  (** Composing the Functors. *)
-  Module QR := FunctorComposeSpec Q R.
-  Include QR.
-
-  Definition pure {A : Type} : A -> F A := R.pure ∘ Q.pure.
-
-  Definition fapp {A B : Type} (f : F (A -> B)) : F A -> F B :=
-    R.fapp (R.fmap Q.fapp f).
-
-  Lemma app_identity : forall {A : Type} (a : F A),
-      fapp (pure (fun x => x)) a = a.
-  Proof.
-    intros. unfold fapp, pure.
-    rewrite R.app_fmap_pure. unfold compose.
-    Search (R.fapp (R.pure _)). rewrite R.app_homomorphism.
-    Search (R.fapp (R.pure _)). rewrite <- R.app_fmap_pure.
-    rewrite <- Q.app_fmap_pure.
-    Search Q.fmap. rewrite Q.fmap_id.
-    rewrite R.fmap_id. reflexivity.
-  Qed.
-
-  Lemma app_homomorphism : forall {A B : Type} (f : A -> B) (a : A),
-      fapp (pure f) (pure a) = pure (f a).
-  Proof.
-    intros. unfold fapp, pure.
-    rewrite R.app_fmap_pure. unfold compose.
-    repeat rewrite R.app_homomorphism. apply f_equal.
-    apply Q.app_homomorphism.
-  Qed.
-
-  Lemma app_interchange : forall {A B : Type} (f : F (A -> B)) (a : A),
-      fapp f (pure a) = fapp (pure (fun h => h a)) f.
-  Proof.
-    intros. unfold fapp, pure. unfold compose.
-    repeat rewrite R.app_fmap_pure.
-    repeat rewrite R.app_homomorphism.
-    rewrite R.app_interchange. Search (R.fapp (R.pure _)).
-    repeat rewrite <- R.app_fmap_pure. Search R.fmap.
-    pose proof @R.fmap_compose as H. unfold compose in H.
-    pose proof H _ _ _ Q.fapp (fun h : Q.F A -> Q.F B => h (Q.pure a)) as H'.
-    Check equal_f. apply equal_f with f in H'.
-    rewrite <- H'; clear H' H. apply f_2_arg.
-    extensionality q. apply Q.app_interchange.
-  Qed.
-
-  Lemma app_composition :
-    forall {A B C : Type} (f : F (A -> B)) (h : F (B -> C)) (a : F A),
-      fapp h (fapp f a) = fapp (fapp (fapp (pure (@compose A B C)) h) f) a.
-  Proof.
-    intros. unfold fapp, pure.
-    repeat rewrite R.app_fmap_pure.
-    repeat rewrite R.app_composition with (h0 := R.pure Q.fapp).
-    repeat rewrite R.app_homomorphism.
-    rewrite R.app_composition with (R.fapp (R.pure Q.fapp) f)
-                                   (R.fapp (R.pure Q.fapp) h) a.
-    apply f_2_arg. repeat rewrite R.app_homomorphism.
-    repeat rewrite <- R.app_fmap_pure.
-    pose proof @R.fmap_compose as H. unfold compose in H.
-    pose proof H _ _ _ Q.fapp (@compose (Q.F A) (Q.F B) (Q.F C)) as H'.
-    apply equal_f with h in H'. rewrite <- H'; clear H'.
-    epose proof H _ _ _ Q.fapp (compose Q.fapp) as H'.
-    apply equal_f with ((R.pure ∘ Q.pure) (@compose A B C)) in H'.
-    rewrite <- H'; clear H'. repeat rewrite R.app_fmap_pure.
-    repeat rewrite R.app_composition. apply f_2_arg.
-    repeat rewrite R.app_homomorphism.
-    repeat rewrite R.app_interchange.
-    unfold compose. repeat rewrite R.app_homomorphism.
-    repeat rewrite <- R.app_fmap_pure.
-    pose proof H _ _ _
-         (fun (a0 : Q.F (B -> C))
-            (f0 : Q.F (A -> B) -> Q.F A -> Q.F B)
-            (a1 : Q.F (A -> B)) (a2 : Q.F A)
-          => Q.fapp a0 (f0 a1 a2))
-         (fun h0 : (Q.F (A -> B) -> Q.F A -> Q.F B) ->
-                 Q.F (A -> B) -> Q.F A -> Q.F C => h0 Q.fapp) as H'.
-    apply equal_f with h in H'. rewrite <- H'; clear H'. apply f_2_arg.
-    extensionality q. extensionality r. extensionality s.
-    rewrite Q.app_composition. reflexivity.
-  Qed.
-
-  Lemma app_fmap_pure : forall {A B : Type} (f : A -> B),
-      fmap f = fapp (pure f).
-  Proof.
-    intros. unfold fapp, fmap, pure. unfold compose.
-    Search R.fmap. repeat rewrite R.app_fmap_pure.
-    apply f_equal. symmetry. rewrite Q.app_fmap_pure.
-    apply R.app_homomorphism.
-  Qed.
-End ApplicativeCompose.
 
 (** * Parameterized Applicatives *)
 
@@ -656,3 +476,153 @@ Module ContApplicativeFactory := ParamApplicativeFactory ContApplicativeSpec.
 Instance ContApplicative (R : Type) : Applicative (cont R) :=
   ContApplicativeFactory.ParamApplicativeInstance R.
 (**[]*)
+
+(** * Applicative Composition *)
+
+Section ApplicativeComposition.
+  Context {Q R : Type -> Type}.
+
+  Context {QF : Functor Q}.
+
+  Context {RF : Functor R}.
+
+  Instance QRFunctor : Functor (R ∘ Q) := ComposeFunctor Q R.
+
+  Context {QA : Applicative Q}.
+
+  Context {RA : Applicative R}.
+
+  Definition purec {A : Type} : A -> (R ∘ Q) A :=
+    (@pure R _ _ _) ∘ (@pure Q _ _ A).
+  (**[]*)
+
+  Definition fappc {A B : Type}
+             (f : (R ∘ Q) (A -> B)) (a : (R ∘ Q) A) : (R ∘ Q) B :=
+    @fapp Q _ _ _ _ <$> f <*> a.
+    (* @fapp R _ _ _ _ (@fmap R _ _ _ (@fapp Q _ _ _ _) f). *)
+  (**[]*)
+
+  Lemma app_identityc : forall {A : Type} (a : (R ∘ Q) A),
+      fappc (purec (fun x => x)) a = a.
+  Proof.
+    intros. unfold fappc, purec.
+    rewrite app_fmap_pure. unfold compose.
+    rewrite app_homomorphism. repeat rewrite <- app_fmap_pure.
+    repeat rewrite fmap_id. reflexivity.
+  Qed.
+
+  Lemma app_homomorphismc : forall {A B : Type} (f : A -> B) (a : A),
+      fappc (purec f) (purec a) = purec (f a).
+  Proof.
+    intros. unfold fappc, purec.
+    rewrite app_fmap_pure. unfold compose.
+    repeat rewrite app_homomorphism. reflexivity.
+  Qed.
+
+  Lemma app_interchangec : forall {A B : Type} (f : (R ∘ Q) (A -> B)) (a : A),
+      fappc f (purec a) = fappc (purec (fun h => h a)) f.
+  Proof.
+    intros. unfold fappc, purec. unfold compose.
+    repeat rewrite app_fmap_pure.
+    repeat rewrite app_homomorphism.
+    rewrite app_interchange.
+    repeat rewrite <- app_fmap_pure.
+    pose proof @fmap_compose R RF as H. unfold compose in H.
+    pose proof H _ _ _ fapp (fun h : Q A -> Q B => h (pure a)) as H'.
+    Check equal_f. apply equal_f with f in H'.
+    rewrite <- H'; clear H' H. apply f_2_arg.
+    extensionality q. rewrite app_interchange.
+    rewrite app_fmap_pure. reflexivity.
+  Qed.
+End ApplicativeComposition.
+
+Module ApplicativeCompose (Q R : ApplicativeSpec) <: ApplicativeSpec.
+  (** Composing the Functors. *)
+  Module QR := FunctorComposeSpec Q R.
+  Include QR.
+
+  Definition pure {A : Type} : A -> F A := R.pure ∘ Q.pure.
+
+  Definition fapp {A B : Type} (f : F (A -> B)) : F A -> F B :=
+    R.fapp (R.fmap Q.fapp f).
+
+  Lemma app_identity : forall {A : Type} (a : F A),
+      fapp (pure (fun x => x)) a = a.
+  Proof.
+    intros. unfold fapp, pure.
+    rewrite R.app_fmap_pure. unfold compose.
+    Search (R.fapp (R.pure _)). rewrite R.app_homomorphism.
+    Search (R.fapp (R.pure _)). rewrite <- R.app_fmap_pure.
+    rewrite <- Q.app_fmap_pure.
+    Search Q.fmap. rewrite Q.fmap_id.
+    rewrite R.fmap_id. reflexivity.
+  Qed.
+
+  Lemma app_homomorphism : forall {A B : Type} (f : A -> B) (a : A),
+      fapp (pure f) (pure a) = pure (f a).
+  Proof.
+    intros. unfold fapp, pure.
+    rewrite R.app_fmap_pure. unfold compose.
+    repeat rewrite R.app_homomorphism. apply f_equal.
+    apply Q.app_homomorphism.
+  Qed.
+
+  Lemma app_interchange : forall {A B : Type} (f : F (A -> B)) (a : A),
+      fapp f (pure a) = fapp (pure (fun h => h a)) f.
+  Proof.
+    intros. unfold fapp, pure. unfold compose.
+    repeat rewrite R.app_fmap_pure.
+    repeat rewrite R.app_homomorphism.
+    rewrite R.app_interchange. Search (R.fapp (R.pure _)).
+    repeat rewrite <- R.app_fmap_pure. Search R.fmap.
+    pose proof @R.fmap_compose as H. unfold compose in H.
+    pose proof H _ _ _ Q.fapp (fun h : Q.F A -> Q.F B => h (Q.pure a)) as H'.
+    Check equal_f. apply equal_f with f in H'.
+    rewrite <- H'; clear H' H. apply f_2_arg.
+    extensionality q. apply Q.app_interchange.
+  Qed.
+
+  Lemma app_composition :
+    forall {A B C : Type} (f : F (A -> B)) (h : F (B -> C)) (a : F A),
+      fapp h (fapp f a) = fapp (fapp (fapp (pure (@compose A B C)) h) f) a.
+  Proof.
+    intros. unfold fapp, pure.
+    repeat rewrite R.app_fmap_pure.
+    repeat rewrite R.app_composition with (h0 := R.pure Q.fapp).
+    repeat rewrite R.app_homomorphism.
+    rewrite R.app_composition with (R.fapp (R.pure Q.fapp) f)
+                                   (R.fapp (R.pure Q.fapp) h) a.
+    apply f_2_arg. repeat rewrite R.app_homomorphism.
+    repeat rewrite <- R.app_fmap_pure.
+    pose proof @R.fmap_compose as H. unfold compose in H.
+    pose proof H _ _ _ Q.fapp (@compose (Q.F A) (Q.F B) (Q.F C)) as H'.
+    apply equal_f with h in H'. rewrite <- H'; clear H'.
+    epose proof H _ _ _ Q.fapp (compose Q.fapp) as H'.
+    apply equal_f with ((R.pure ∘ Q.pure) (@compose A B C)) in H'.
+    rewrite <- H'; clear H'. repeat rewrite R.app_fmap_pure.
+    repeat rewrite R.app_composition. apply f_2_arg.
+    repeat rewrite R.app_homomorphism.
+    repeat rewrite R.app_interchange.
+    unfold compose. repeat rewrite R.app_homomorphism.
+    repeat rewrite <- R.app_fmap_pure.
+    pose proof H _ _ _
+         (fun (a0 : Q.F (B -> C))
+            (f0 : Q.F (A -> B) -> Q.F A -> Q.F B)
+            (a1 : Q.F (A -> B)) (a2 : Q.F A)
+          => Q.fapp a0 (f0 a1 a2))
+         (fun h0 : (Q.F (A -> B) -> Q.F A -> Q.F B) ->
+                 Q.F (A -> B) -> Q.F A -> Q.F C => h0 Q.fapp) as H'.
+    apply equal_f with h in H'. rewrite <- H'; clear H'. apply f_2_arg.
+    extensionality q. extensionality r. extensionality s.
+    rewrite Q.app_composition. reflexivity.
+  Qed.
+
+  Lemma app_fmap_pure : forall {A B : Type} (f : A -> B),
+      fmap f = fapp (pure f).
+  Proof.
+    intros. unfold fapp, fmap, pure. unfold compose.
+    Search R.fmap. repeat rewrite R.app_fmap_pure.
+    apply f_equal. symmetry. rewrite Q.app_fmap_pure.
+    apply R.app_homomorphism.
+  Qed.
+End ApplicativeCompose.
