d9bf24edea41deffc3da626e33aed560c610c78c
rnp39 <rnp39@cornell.edu>
2021-01-08 15:11:13 -0800

    traversable lists with (reasonable) axiom

--- Traversable.v
+++ Traversable.v
@@ -2,6 +2,24 @@ Require Import Coq.Lists.List.
 Require Export TypeClassLib.Applicative.
 Require Export TypeClassLib.Foldable.
 
+Lemma fapp_compose :
+  forall {F : Type -> Type} `{Applicative F} {A B C : Type}
+    (f : A -> B) (h : B -> C),
+    fapp (pure (h ∘ f)) = fapp (pure h) ∘ fapp (pure f).
+Proof.
+  intros. repeat rewrite <- app_fmap_pure.
+  apply fmap_compose.
+Qed.
+
+Lemma fapp_compose_unfolded :
+  forall {F : Type -> Type} `{FA : Applicative F} {A B C : Type}
+    (f : A -> B) (h : B -> C) (t : F A),
+    fapp (pure (fun a => h (f a))) t = fapp (pure h) ((fapp (pure f)) t).
+Proof.
+  intros. pose proof fapp_compose f h as H.
+  unfold compose in H. rewrite H. reflexivity.
+Qed.
+
 (** * The Traversable Type Class *)
 Class Traversable (T : Type -> Type) :=
   { Traversable_Applicative :> Applicative T;
@@ -23,7 +41,7 @@ Module Type TraversableSpec.
   (** The Traversable. *)
   Parameter T : Type -> Type.
 
-  (** Functor Requirement. *)
+  (** Applicative Requirement. *)
   Parameter TApplicative : Applicative T.
 
   (** Foldable Requirement. *)
@@ -135,4 +153,80 @@ End OptionTraversableSpec.
 Module OptionTraversableFactory := TraversableFactory OptionTraversableSpec.
 Instance OptionTraversable : Traversable option :=
   OptionTraversableFactory.TraversableInstance.
+(**[]*)
+
+(** Lists. *)
+Module ListTraversableSpec <: TraversableSpec.
+  Import Coq.Lists.List.
+  Import ListNotations.
+
+  Definition T : Type -> Type := list.
+
+  Definition TApplicative : Applicative list := ListApplicative.
+
+  Definition TFoldable : Foldable list := ListFoldable.
+
+  Fixpoint traverse {F : Type -> Type} `{Applicative F}
+           {A B : Type} (f : A -> F B) (l : list A) : F (list B) :=
+    match l with
+    | []    => pure []
+    | a :: l => cons <$> f a <*> traverse f l
+    end.
+  (**[]*)
+
+  Lemma traverse_id : forall {A : Type},
+      @traverse (fun X => X) IdentityApplicative _ _
+                (fun a : A => a) = fun t : list A => t.
+  Proof.
+    intros. extensionality t.
+    induction t as [| h t IHt]; simpl; auto.
+    rewrite IHt. reflexivity.
+  Qed.
+  (**[]*)
+
+  (** This is true in my heart of hearts. *)
+  Axiom switch_fun_args_app :
+    forall {F : Type -> Type} `{Applicative F}
+      {A B C : Type} (f : A -> B -> C) (a : F A) (b : F B),
+      (fun x y => f x y) <$> a <*> b = (fun y x => f x y) <$> b <*> a.
+
+  (** Traversals compose. *)
+  Lemma traverse_compose :
+    forall {Q R : Type -> Type} `{QA : Applicative Q} `{RA : Applicative R}
+      {A B C : Type} (f : A -> Q B) (h : B -> R C),
+      @traverse (Q ∘ R) _ _ _ (@fmap Q _ B (R C) h ∘ f) =
+      @fmap Q _ (list B) (R (list C)) (@traverse R _ B C h) ∘ (@traverse Q _ A B f).
+  Proof.
+    intros. extensionality t.
+    induction t as [| a t IHt]; simpl.
+    - unfold purec. unfold compose. cbn.
+      rewrite app_fmap_pure. rewrite app_homomorphism.
+      reflexivity.
+    - rewrite IHt; clear IHt.
+      unfold fappc, fmapc. unfold compose. cbn.
+      repeat rewrite app_fmap_pure.
+      rewrite app_composition with (h0 := pure (fapp (pure cons))).
+      rewrite app_composition with (h0 := pure (traverse h)).
+      rewrite app_composition with (f0 := pure (traverse h)).
+      rewrite app_composition with (h0 := pure fapp).
+      apply f_2_arg. clear t. repeat rewrite app_homomorphism.
+      repeat rewrite app_composition with (a0 := f a).
+      repeat rewrite app_homomorphism.
+      repeat rewrite fapp_compose. unfold compose.
+      rewrite app_composition with (f0 := pure cons).
+      rewrite app_composition with (f0 := pure h). rewrite app_composition.
+      repeat rewrite app_homomorphism. unfold compose. cbn.
+      rewrite app_fmap_pure. rewrite app_composition.
+      repeat rewrite app_homomorphism. unfold compose.
+      repeat rewrite <- app_fmap_pure.
+      pose proof switch_fun_args_app
+          (fun x y a1 => cons <$> h x <*> y a1) (f a) (pure (traverse h)) as H.
+      simpl in H. rewrite H. rewrite app_fmap_pure.
+      rewrite app_homomorphism. rewrite <- app_fmap_pure. reflexivity.
+  Qed.
+End ListTraversableSpec.
+
+Module ListTraversableFactory := TraversableFactory ListTraversableSpec.
+Instance ListTraversable : Traversable list :=
+  ListTraversableFactory.TraversableInstance.
 (**[]*)
