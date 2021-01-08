31eb17e4b4afd958f5bbe9f0e8f657fe672b13c9
rnp39 <rnp39@cornell.edu>
2021-01-07 16:29:46 -0800

    cleared traverse_id definition

--- Traversable.v
+++ Traversable.v
@@ -7,12 +7,14 @@ Class Traversable (T : Type -> Type) `{Functor T} `{Foldable T} :=
   { traverse : forall {F : Type -> Type} `{Applicative F}
                  {A B : Type}, (A -> F B) -> T A -> F (T B);
     (** Laws. *)
-    (* traverse_id : forall {A : Type},
-      traverse (fun a : A => a) = fun t : T A => t; *)
-    traverse_compose :
+    traverse_id : forall {A : Type},
+        @traverse (fun X : Type => X) IdentityFunctor _ _ _
+                  (fun a : A => a) = fun t : T A => t;
+    (*traverse_compose :
       forall {F : Type -> Type} `{Applicative F}
         {A B C : Type} (f : A -> F B) (h : B -> F C),
-        traverse (fmap h ∘ f) = fmap (traverse h) ∘ (traverse f) }.
+        traverse (fmap h ∘ f) = fmap (traverse h) ∘ (traverse f) *)
+    }.
 (**[]*)
 
 Print Traversable.
@@ -34,15 +36,16 @@ Module Type TraversableSpec.
   (** Laws. *)
 
   (** The "identity" traversal. *)
-  (* Parameter traverse_id : forall {B : Type},
-      traverse (fun b : B => b) = fun t : T B => t. *)
+  Parameter traverse_id : forall {B : Type},
+      @traverse (fun X => X) IdentityFunctor _ _ _
+                (fun b : B => b) = fun t : T B => t.
   (**[]*)
 
   (** Traversals compose. *)
-  Parameter traverse_compose :
+  (* Parameter traverse_compose :
     forall {A : Type -> Type} `{Applicative A}
       {B C D : Type} (f : B -> A C) (h : C -> A D),
-      traverse (fmap h ∘ f) = fmap (traverse h) ∘ (traverse f).
+      traverse (fmap h ∘ f) = fmap (traverse h) ∘ (traverse f). *)
   (**[]*)
 End TraversableSpec.
 
@@ -52,8 +55,9 @@ Module TraversableFactory (TS : TraversableSpec).
 
   Instance TraversableInstance : Traversable TS.T :=
   { traverse := @TS.traverse;
-    (* traverse_id := @TS.traverse_id; *)
-    traverse_compose := @TS.traverse_compose }.
+    traverse_id := @TS.traverse_id;
+    (* traverse_compose := @TS.traverse_compose *)
+  }.
 End TraversableFactory.
 
 (** Identity. *)
@@ -68,20 +72,56 @@ Module IdentityTraversableSpec <: TraversableSpec.
              {B C : Type} (f : B -> A C) : B -> A C := f.
   (**[]*)
 
-  (* Lemma traverse_id_pure : forall {B : Type},
-      traverse (fun b : B => b) = (fun b : B => b).
-  Proof. intros. reflexivity. Qed. *)
+  Lemma traverse_id : forall {B : Type},
+      @traverse (fun X => X) IdentityFunctor _ _ _
+                (fun b : B => b) = (fun b : B => b).
+  Proof. intros. reflexivity. Qed.
   (**[]*)
 
-  (** Traversals compose. *)
-  Lemma traverse_compose :
+  (* Lemma traverse_compose :
     forall {A : Type -> Type} `{Applicative A}
       {B C D : Type} (f : B -> A C) (h : C -> A D),
       traverse (fmap h ∘ f) = fmap (traverse h) ∘ (traverse f).
-  Proof. intros. unfold traverse. reflexivity. Qed.
+  Proof. intros. unfold traverse. reflexivity. Qed. *)
   (**[]*)
 End IdentityTraversableSpec.
 
 Module IdentityTraversableFactory := TraversableFactory IdentityTraversableSpec.
 Instance IdentityTraversable : Traversable (fun X => X) :=
   IdentityTraversableFactory.TraversableInstance.
+(**[]*)
+
+(** Option. *)
+Module OptionTraversableSpec <: TraversableSpec.
+  Definition T : Type -> Type := option.
+
+  Instance TFunctor : Functor option := OptionFunctor.
+
+  Instance TFoldable : Foldable option := OptionFoldable.
+
+  Definition traverse {A : Type -> Type} `{Applicative A} {B C : Type}
+             (f : B -> A C) (o : option B) : A (option C) :=
+    match o with
+    | None   => pure None
+    | Some b => Some <$> f b
+    end.
+  (**[]*)
+
+  Lemma traverse_id : forall {B : Type},
+      @traverse (fun X => X) IdentityFunctor _ _ _
+                (fun b : B => b) = (fun o : option B => o).
+  Proof. intros. extensionality o. destruct o; auto. Qed.
+
+  (*Lemma traverse_compose :
+    forall {A : Type -> Type} `{Applicative A}
+      {B C D : Type} (f : B -> A C) (h : C -> A D),
+      traverse (fmap h ∘ f) = fmap (traverse h) ∘ (traverse f).
+  Proof.
+    intros. extensionality o. destruct o as [b |]; simpl.
+    - unfold fmap. *)
+End OptionTraversableSpec.
+
+Module OptionTraversableFactory := TraversableFactory OptionTraversableSpec.
+Instance OptionTraversable : Traversable option :=
+  OptionTraversableFactory.TraversableInstance.
+(**[]*)
